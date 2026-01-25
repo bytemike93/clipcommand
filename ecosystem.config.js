81115b0aa3f800aca8f730a4613e863ec7cf240b
Michael Schwarz <info@bytemike.de>
2026-01-25 15:56:32 +0100

    pm2 logs location

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,7 +15,10 @@ module.exports = {
                 TWITCH_REDIRECT_URI: process.env.TWITCH_REDIRECT_URI,
                 TOKEN_ENCRYPTION_KEY: process.env.TOKEN_ENCRYPTION_KEY,
                 HTTP_TIMEOUT_MS: process.env.HTTP_TIMEOUT_MS || 10000
-            }
+            },
+            merge_logs: true,
+            out_file: "./logs/clipserver-out.log",
+            error_file: "./logs/clipserver-err.log"
         }
     ]
 };
